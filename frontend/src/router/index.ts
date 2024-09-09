e53ebcf0e46465254f7337e9d44491aa87f2dd70
Noroichan <7top.load@gmail.com>
2024-09-09 11:29:35 +0200

    chore: init vue project

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,8 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(), // TODO base url
+  routes: []
+})
+
+export default router
