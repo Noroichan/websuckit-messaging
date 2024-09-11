2b1b652813e32d737ca01c992a43314dc220373e
Noroichan <7top.load@gmail.com>
2024-09-11 14:23:54 +0200

    fix: overflow

--- frontend/src/views/ConversationsView.vue
+++ frontend/src/views/ConversationsView.vue
@@ -13,19 +13,19 @@
 <template>
     <!-- TODO smooth profile pic animation, rethink color scheme -->
     <!-- Chat bubbles -->
-    <section class="float-left bg-blue-950 w-1/12 h-screen border-r-4 border-solid border-indigo-950">
-        <div class="flex flex-col items-center">
-            <img src="../assets/pictures/profile-pic-default.png.png" class="w-16 h-16 m-4 rounded-full hover:rounded-lg" v-for="i in 10" :key="i"/>
+    <section class="float-left bg-blue-950 w-1/20 h-screen border-r-4 border-solid border-indigo-950">
+        <div class="flex flex-col items-center h-4/6 overflow-y-auto">
+            <img src="../assets/pictures/profile-pic-default.png.png" class="w-4/6 max-w-14 m-4 rounded-full hover:rounded-lg" v-for="i in 10" :key="i"/>
         </div>
     </section>
     <!-- Messages -->
-    <section class="float-right flex flex-col items-center h-screen w-11/12">
-        <div class="flex flex-col justify-items-stretch bg-sky-800 flex-grow w-full">
+    <section class="float-right flex flex-col items-center h-screen w-19/20">
+        <div class="flex flex-col justify-items-stretch bg-sky-800 h-9/10 w-full overflow-y-auto">
             <div class="flex" :class="index % 2 ? 'flex-row' : 'flex-row-reverse'" v-for="(msg, index) in testMessages" :key="msg">
                 <div class="text-white py-2 px-3 w-fit max-w-4/5 mx-2 mt-2 rounded-lg" :class="index % 2 ? 'bg-indigo-950' : 'bg-gray-800'">{{ msg }}</div>
             </div>
         </div>
-        <div class="flex flex-row items-center justify-items-center bg-blue-950 h-32 w-full border-t-4 border-solid border-indigo-950">
+        <div class="flex flex-row items-center justify-items-center bg-blue-950 h-1/10 w-full border-t-4 border-solid border-indigo-950">
             <textarea class="flex-grow bg-sky-900 m-4 rounded-lg"></textarea>
         </div>
     </section>
