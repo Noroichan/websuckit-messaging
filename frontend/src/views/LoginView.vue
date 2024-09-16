775357e0ca3523c2dea2b34c8bec5331e34adf28
Noroichan <7top.load@gmail.com>
2024-09-16 17:43:36 +0200

    view: login

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<!-- TODO submit name and check if available -->
+
+<template>
+    <div class="flex justify-center items-center h-screen w-screen bg-slate-900">
+        <div class="flex flex-col justify-center items-center w-2/6 min-w-96 h-2/6 min-h-80 bg-indigo-1000">
+            <label class="text-white text-lg">Username:</label>
+            <input type="text" class="my-4 min-w-44" />
+            <button class="text-white text-lg bg-slate-925 rounded-md w-1/6 min-w-32 py-2">Enter</button>
+        </div>
+    </div>
+</template>
